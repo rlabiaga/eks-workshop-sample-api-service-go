3a9afeb3db3e7693a3a9f94cabce066867bd6b1d
rlabiaga <45471414+rlabiaga@users.noreply.github.com>
2018-11-29 17:02:57 -0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World - updated"}
+		res := &response{Message: "Hello World - updated 2nd time"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
